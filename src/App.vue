e0112511eb8c77707458a6150a4fbfd9c3752579
Shovel <mota.luiz@live.com>
2024-06-03 07:16:27 -0300

    first

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+
+</script>
+
+<template>
+
+ 
+
+
+
+  <RouterView v-slot="{Component}" >
+    <Transition name="fade" mode="out-in">
+      <Component :is="Component"/>
+    </Transition>
+  </RouterView>
+  
+
+  <div class="wrapper">
+
+      <nav>
+        <button  > Change locale</button>
+        <RouterLink to="/">
+          <button > {{ $t('message.hello') }}</button>
+        </RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+</template>
+<style>
+.fade-enter-from,.fade-leave-to{
+  opacity: 0;
+}
+.fade-enter-active,.fade-leave-active{
+  transition: opacity 0.5s ease-out ;
+}
+</style>
+
---
+++ src/main.js
@@ -0,0 +1,28 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import { createI18n } from 'vue-i18n'
+
+import App from './App.vue'
+import router from './router'
+
+
+import Portuguese from "./translations/Portuguese.json"
+import English from "./translations/English.json"
+const i18n = createI18n({
+    locale: navigator.language,
+    fallbackLocale: 'en',
+    messages: {
+        en: English,
+        pt: Portuguese,
+    },
+})
+
+const app = createApp(App)
+
+app.use(i18n)
+app.use(createPinia())
+app.use(router)
+
+app.mount('#app')
